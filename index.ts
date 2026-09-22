fe10c4484ced6a33340aa25e1234966154652743
simonabrezza81-blip <simonabrezza81@gmail.com>
2026-09-22 17:45:47 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,13 +7,11 @@ const startingHeader = {
     "User-Agent": "CVVS/std/4.2.3 Android/12",
     "Z-Dev-Apikey": "Tg1NWEwNGIgIC0K",
     "Content-Type": "application/json",
-}
+};
 
 const printBanner = () => {
-    console.log("\r\n   _____    ___   _____      _ \r\n  \/ ____|  |__ \\ \/ ____|    | |\r\n | |  __   __ ) | |     __ _| |\r\n | |  \\ \\ \/ \/\/ \/| |    \/ _` | |\r\n | |___\\ V \/\/ \/_| |___| (_| | |\r\n  \\_____\\_\/|____|\\_____\\__,_|_|\r\n");
-    console.log("View the Classeviva agenda in an actual calendar app. 📆");
-    console.log("Made by SysWhite, ⭐️ star the repo on Github if you like it!\n");
-}
+    console.log("Classeviva2Cal - aggiornamento calendario");
+};
 
 const getUserAuth = async (): Promise<{ token: string, uid: string }> => {
     const tokenRequest = await fetch("https://web.spaggiari.eu/rest/v1/auth/login", {
@@ -34,41 +32,52 @@ const getUserAuth = async (): Promise<{ token: string, uid: string }> => {
     }
 
     const tokenResponse = await tokenRequest.json() as { token: string, ident: string };
+
     console.log("✅ Successfully authenticated with classeviva.");
 
     return {
         token: tokenResponse.token,
         uid: tokenResponse.ident.slice(1, -1)
-    }
-}
+    };
+};
 
 const getAgendaInterval = async (token: string, uid: string): Promise<string[]> => {
     try {
-        const response = await fetch(`https://web.spaggiari.eu/rest/v1/students/${uid}/periods`, {
-            headers: {
-                ...startingHeader,
-                "Z-Auth-Token": token
-            },
-            method: "GET"
-        });
+        const response = await fetch(
+            `https://web.spaggiari.eu/rest/v1/students/${uid}/periods`,
+            {
+                headers: {
+                    ...startingHeader,
+                    "Z-Auth-Token": token
+                },
+                method: "GET"
+            }
+        );
 
-        const responseData: any = (await response.json() as any).periods
+        const responseData: any = (await response.json() as any).periods;
+
         const periodTimespans: string[] = [
             responseData[0]?.dateStart.slice(0.10).replace(/-/g, ''),
             responseData[responseData.length - 1]?.dateEnd.slice(0.10).replace(/-/g, '')
         ];
 
         return periodTimespans;
+
     } catch (error) {
         console.error("❌ Failed to get agenda interval:", error);
+
         return [
             new Date().toISOString().slice(0, 10).replace(/-/g, ''),
             new Date().toISOString().slice(0, 10).replace(/-/g, '')
         ];
     }
-}
+};
 
-const getAgendaItems = async (token: string, uid: string): Promise<ics.EventAttributes[]> => {
+const getAgendaItems = async (
+    token: string,
+    uid: string
+): Promise<ics.EventAttributes[]> => {
+
     const agendaIntervals = await getAgendaInterval(token, uid);
 
     const agendaRequest = await fetch(
@@ -86,7 +95,9 @@ const getAgendaItems = async (token: string, uid: string): Promise<ics.EventAttr
         throw new Error("❌ Failed to fetch agenda items.");
     }
 
-    const agenda = (await agendaRequest.json() as { agenda: any[] }).agenda as {
+    const agenda = (
+        await agendaRequest.json() as { agenda: any[] }
+    ).agenda as {
         evtId: number;
         evtCode: string;
         evtDatetimeBegin: string;
@@ -105,94 +116,136 @@ const getAgendaItems = async (token: string, uid: string): Promise<ics.EventAttr
     const eventsRegistry = await readJsonRegistry();
 
     agenda.map((event) => {
-        const alreadySeenEvent = eventsRegistry.events.find((e) => e.evtId == event.evtId);
+        const alreadySeenEvent = eventsRegistry.events.find(
+            (e) => e.evtId == event.evtId
+        );
 
         if (!alreadySeenEvent) {
-            eventsRegistry.events = [...eventsRegistry.events, {
-                evtId: event.evtId,
-                firstSeenDate: new Date()
-            }];
+            eventsRegistry.events = [
+                ...eventsRegistry.events,
+                {
+                    evtId: event.evtId,
+                    firstSeenDate: new Date()
+                }
+            ];
         }
     });
 
     writeJsonRegistry(eventsRegistry);
 
-    return agenda
-        .map((event) => {
-            const start = new Date(event.evtDatetimeBegin);
-            const end = new Date(event.evtDatetimeEnd);
+    return agenda.map((event) => {
+        const start = new Date(event.evtDatetimeBegin);
+        const end = new Date(event.evtDatetimeEnd);
 
-            const isAllDay =
-                start.getHours() == 0 &&
-                (
-                    end.getHours() == 23 && end.getMinutes() == 59 ||
-                    end.getHours() == 0 && end.getMinutes() == 0
-                );
+        const isAllDay =
+            start.getHours() == 0 &&
+            (
+                (end.getHours() == 23 && end.getMinutes() == 59) ||
+                (end.getHours() == 0 && end.getMinutes() == 0)
+            );
 
-            return {
-                title: event.subjectDesc || event.authorName,
-                organizer: {
-                    name: event.authorName,
-                    email: `${event.authorName.toLocaleLowerCase().replace(/ /g, '.')}@syswhite.dev`
-                },
-                description: `${event.notes}\n\n-----------------\nEvent first seen on Classeviva on ${new Date(
-                    eventsRegistry.events.find(e => e.evtId == event.evtId)?.firstSeenDate || new Date()
-                ).toLocaleString("it-IT", { timeZone: "Europe/Rome" })}\n\nLast synced with Classeviva on ${new Date().toLocaleString(
-                    "it-IT",
-                    { timeZone: "Europe/Rome" }
-                )}\n-----------------\n\nClasseviva2Cal Made with ❤️ by SysWhite.`,
-                busyStatus: "FREE" as "FREE" | "TENTATIVE" | "BUSY" | "OOF",
-                start: isAllDay
-                    ? [
-                        start.getFullYear(),
-                        start.getMonth() + 1,
-                        start.getDate()
-                    ] as [number, number, number]
-                    : [
-                        start.getFullYear(),
-                        start.getMonth() + 1,
-                        start.getDate(),
-                        start.getHours(),
-                        start.getMinutes()
-                    ] as [number, number, number, number, number],
-                duration: {
-                    minutes: Math.round((end.getTime() - start.getTime()) / 60000)
-                },
-            };
-        });
-}
+        return {
+            title: event.subjectDesc || event.authorName,
+
+            organizer: {
+                name: event.authorName,
+                email: `${event.authorName
+                    .toLocaleLowerCase()
+                    .replace(/ /g, '.')}@syswhite.dev`
+            },
+
+            description: `${event.notes}
+
+-----------------
+Event first seen on Classeviva on ${new Date(
+                eventsRegistry.events.find(
+                    e => e.evtId == event.evtId
+                )?.firstSeenDate || new Date()
+            ).toLocaleString("it-IT", {
+                timeZone: "Europe/Rome"
+            })}
+
+Last synced with Classeviva on ${new Date().toLocaleString(
+                "it-IT",
+                { timeZone: "Europe/Rome" }
+            )}
+-----------------
+
+Classeviva2Cal Made with ❤️ by SysWhite.`,
+
+            busyStatus: "FREE" as "FREE" | "TENTATIVE" | "BUSY" | "OOF",
+
+            start: isAllDay
+                ? [
+                    start.getFullYear(),
+                    start.getMonth() + 1,
+                    start.getDate()
+                ] as [number, number, number]
+
+                : [
+                    start.getFullYear(),
+                    start.getMonth() + 1,
+                    start.getDate(),
+                    start.getHours(),
+                    start.getMinutes()
+                ] as [number, number, number, number, number],
+
+            duration: {
+                minutes: Math.round(
+                    (end.getTime() - start.getTime()) / 60000
+                )
+            },
+        };
+    });
+};
 
 const updateAgendaCalendarFile = async () => {
+
     const { token, uid } = await getUserAuth();
 
     ics.createEvents(
-        (await getAgendaItems(token, uid)),
+        await getAgendaItems(token, uid),
         (error: any, value: any) => {
+
             if (error) {
                 console.log("❌ Failed to update calendar file.");
             } else {
                 console.log("✅ Successfully updated calendar file.");
             }
 
-            writeFileSync(`${__dirname}/output/agenda.ics`, value)
+            writeFileSync(
+                `${__dirname}/output/agenda.ics`,
+                value
+            );
         }
-    )
-}
+    );
+};
 
 const initializeJsonRegistry = async () => {
+
     try {
-        await fs.promises.access('registry.json', fs.constants.F_OK);
+        await fs.promises.access(
+            'registry.json',
+            fs.constants.F_OK
+        );
+
     } catch {
+
         try {
             await writeJsonRegistry({
                 "events": []
             });
+
             console.log("✅ Initialized json registry.");
+
         } catch {
-            throw new Error("❌ Failed to initialize registry file.");
+
+            throw new Error(
+                "❌ Failed to initialize registry file."
+            );
         }
     }
-}
+};
 
 const readJsonRegistry = async (): Promise<{
     "events": {
@@ -200,25 +253,64 @@ const readJsonRegistry = async (): Promise<{
         "firstSeenDate": Date
     }[]
 }> => {
+
     try {
-        const registryFile = await fs.promises.readFile('registry.json', 'utf-8');
+
+        const registryFile = await fs.promises.readFile(
+            'registry.json',
+            'utf-8'
+        );
+
         const jsonData = JSON.parse(registryFile);
+
         return jsonData;
+
     } catch {
-        throw new Error("❌ Failed to read from registry file.")
+
+        throw new Error(
+            "❌ Failed to read from registry file."
+        );
     }
+};
+
+const writeJsonRegistry = async (
+    newRegistryData: {
+        "events": {
+            "evtId": number,
+            "firstSeenDate": Date
+        }[]
+    }
+) => {
+
+    try {
+
+        await fs.promises.writeFile(
+            'registry.json',
+            JSON.stringify(newRegistryData)
+        );
+
+    } catch {
+
+        throw new Error(
+            "❌ Failed to write to registry file."
+        );
+    }
+};
+
+printBanner();
+
+initializeJsonRegistry();
+
+if (
+    !process.env.CLASSEVIVA_PASSWORD ||
+    !process.env.CLASSEVIVA_USERNAME
+) {
+    console.error(
+        "❌ Please set the CLASSEVIVA_PASSWORD and CLASSEVIVA_USERNAME environment variables."
+    );
+
+    process.exit(1);
 }
 
-const writeJsonRegistry = async (newRegistryData: {
-    "events": {
-        "evtId": number,
-        "firstSeenDate": Date
-    }[]
-}) => {
-    try {
-        await fs.promises.writeFile('registry.json', JSON.stringify(newRegistryData));
-    } catch {
-        throw new Error("❌ Failed to write to registry file.")
+updateAgendaCalendarFile();
 ```
-
-
